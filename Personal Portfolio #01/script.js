d48e874834640994049915219b559c86b0498ae0
SantiSosa02 <santi4586sosa@gmail.com>
2024-05-18 14:20:47 -0500

    first commit

--- Personal Portfolio #01/script.js
+++ Personal Portfolio #01/script.js
@@ -1,26 +1,30 @@
 function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
+    // Selecciona todos los elementos que coincidan con el selector dado
     const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
+    // Verifica si hay elementos que coincidan con el selector y si existe la instancia solicitada
     if (elements.length > instance) {
-        // Scroll to the specified instance of the element
+        // Desplaza hasta la instancia especificada del elemento
         elements[instance].scrollIntoView({ behavior: 'smooth' });
     }
 }
 
+// Obtiene los elementos de enlace
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
+// Agrega eventos de clic a los elementos de enlace
 link1.addEventListener('click', () => {
+    // Desplaza hasta el primer elemento con la clase "header"
     scrollToElement('.header');
 });
 
 link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
+    // Desplaza hasta el segundo elemento con la clase "header"
     scrollToElement('.header', 1);
 });
 
 link3.addEventListener('click', () => {
+    // Desplaza hasta el primer elemento con la clase "column"
     scrollToElement('.column');
 });
